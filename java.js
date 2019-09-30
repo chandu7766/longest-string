26e0cfbd1009964f78433e00dfb9be5ab4254a12
saichandugampa <gampa.saichandu95@gmail.com>
2019-09-30 13:17:32 -0500

    updated with string values

--- java.js
+++ java.js
@@ -1,11 +1,11 @@
 function long(){
-    const str1=document.getElementById("first")
-    const str2=document.getElementById("second")
-    console.log(str1);
-    console.log(str2);
-
+    const str1=document.getElementById("first").value
+    const str2=document.getElementById("second").value
     const longest= Math.max(str1.length, str2.length);
-    document.getElementById("result").innerHTML="The longest string is: "+longest;
+    if(str1.length>str2.length)
+    document.getElementById("result").innerHTML="The longest string length is: "+longest+" that is str1 with value "+str1;
+    if(str2.length>str1.length)
+    document.getElementById("result").innerHTML="The longest string length is: "+longest+" that is str2 with value "+str2;
 
 
 }
